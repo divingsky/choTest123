4f4d6129d0689b827d57814942a9fa4069cc6078
owner <divingsky@hanmail.net>
2015-10-26 10:01:26 +0900

    aaasas

---
+++ app/src/main/java/com/example/owner/ex3/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.owner.ex3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("시작","시작");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("온스타트","온스타트");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("일시정지","일시정지");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("멈춤", "멈춤");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("온스탑", "온스탑");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("끝남", "끝남");
+    }
+}
